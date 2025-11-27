a6cf4c75f91e47321eef629cb777c27b513127dc
adiva13 <sitiadivakirani05majene@gmail.com>
2025-11-27 13:44:39 +0800

    Create luas_segiempat.py

---
+++ luas_segiempat.py
@@ -0,0 +1,12 @@
+#nama = Siti Adiva Kirani
+#Nim = D0425302
+#kelas = B_SistemInformasi
+
+print('## program python menghitung luas segiempat ##')
+print('==============================================')
+
+panjang = float(input('input panjang segimepat: '))
+lebar = float(input('input luas segiempat: '))
+
+luas = panjang * lebar
+print('luas segiempat = ',round(luas,2))
