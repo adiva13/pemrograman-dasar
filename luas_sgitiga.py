d039bcc161b786aee5c7d1460fc6ba6bdb670d33
adiva13 <sitiadivakirani05majene@gmail.com>
2025-11-27 13:25:39 +0800

    Create luas_sgitiga.py

---
+++ luas_sgitiga.py
@@ -0,0 +1,12 @@
+#nama : Siti Adiva Kirani
+#Nim : D0425302
+#kelas : B_SistemInformasi
+
+print('## program python menghitung luas segitiga ##')
+print('=============================================')
+
+alas = float(input('input alas segitiga: '))
+tinggi = float(input('input tinggi segitiga: '))
+
+luas = 0.5 * alas * tinggi
+print('luas segitiga = ',round(luas,2))
